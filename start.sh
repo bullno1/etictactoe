b8e1c2e8fe0f3e7cd07071b29ac1de3cf1219f8c
bullno1 <thebullno1@gmail.com>
2014-02-10 01:31:45 +0800

    Make start.sh executable and more readable

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-erl -boot start_sasl -pa `pwd`/deps/*/ebin `pwd`/ebin -eval "application:ensure_all_started(etictactoe), application:ensure_all_started(sync)"
+erl -boot start_sasl \
+    -pa `pwd`/deps/*/ebin `pwd`/ebin \
+    -eval "application:ensure_all_started(etictactoe), application:ensure_all_started(sync)"
